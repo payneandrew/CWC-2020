c00c70685703d1a5f116cc0474cc48334e0c4982
ElliottMCG <48140818+ElliottMCG@users.noreply.github.com>
2020-02-17 15:10:57 -0500

    Update CS1.ino

--- CS1/CS1.ino
+++ CS1/CS1.ino
@@ -1,33 +1,31 @@
-const int currentPin = A0;
-int sensitivity = 185;
-int adcValue= 0;
-int offsetVoltage = 2500;
-double adcVoltage = 0;
-double currentValue = 0;
-
 void setup() {
-  // put your setup code here, to run once:
 Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  current();
+}
+
+int current() {
+  // Declare Variables
+  const int currentPin = A0;
+  int sensitivity = 185;
+  int adcValue= 0;
+  int offsetVoltage = 2500;
+  double adcVoltage = 0;
+  double currentValue = 0;
+
+  // Function
   adcValue = analogRead(currentPin);
   adcVoltage = (adcValue / 1024.0) * 5000;
   currentValue = ((adcVoltage - offsetVoltage) / sensitivity);
-  
+  // Print Statements
   Serial.print("Raw Sensor Value = " );
   Serial.print(adcValue);
-
-  //delay(2000);
-
   Serial.print("\t Voltage(mV) = ");
   Serial.print(adcVoltage,3);
-  
-  //delay(2000);
- 
   Serial.print("\t Current(A) = ");
   Serial.println(currentValue,3);
- 
+  // 2.5s delay
   delay(2500);
 }
